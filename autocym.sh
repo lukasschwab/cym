23a7696be5dfca8c257a8e9ba0dce6409a07c533
Lukas Schwab <lukas.schwab@gmail.com>
2016-11-28 00:21:55 -0800

    Add Squeeze: Up the Junction

---
+++ autocym.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd ~/Desktop/Programming\ Projects/cym/
+# If there's a change to index.html
+if ! git diff-index --quiet HEAD -- index.html; then
+    # Push any changes in index.html to github
+    git add index.html
+    git commit --quiet -m 'auto cym update'
+    git push --quiet
+    /usr/local/bin/terminal-notifier -title 'cym' -message 'Automatically updated'  -open 'http://lukasschwab.github.io/cym' -sound default
+fi
